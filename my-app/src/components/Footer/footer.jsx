9e3ab1430b8b16da2c9e5e7a4648ef8b54cfa97a
Roman Chaus <roman1997lviv@gmail.com>
2022-06-22 17:41:05 +0300

    footer markup and styling added

---
+++ my-app/src/components/Footer/footer.jsx
@@ -0,0 +1,78 @@
+import './footer.css';
+import React from "react";
+import instagram from '../../img/instagram.svg';
+import facebook from '../../img/facebook.svg';
+import Linkedin from '../../img/Linkedin.svg';
+import Twitter from '../../img/Twitter.svg';
+import logo from '../../img/logo.svg';
+
+export const Footer = (props) => {
+    return (
+        <footer>
+            <div className="wrapper">
+                <div className="top-line">
+                    <div className='company-desc'>
+                        <img src={logo} alt="" />
+                        <p>The world’s first and largest digital marketplace for crypto collectibles and non-fungible tokens (NFTs). Buy, sell, and discover exclusive digital items.</p>
+                    </div>
+                    <div className='links'>
+                        <h3>Marketplace</h3>
+                        <a href="">All NFTs</a>
+                        <a href="">Art</a>
+                        <a href="">Collectibles</a>
+                        <a href="">Domain Names</a>
+                        <a href="">Music</a>
+                        <a href="">Photograhpy</a>
+                        <a href="">Sports</a>
+                        <a href="">Trading cards</a>
+                        <a href="">Utility</a>
+                        <a href="">Virtual Words</a>
+                    </div>
+                    <div className='links'>
+                        <h3>My Account</h3>
+                        <a href="">Profile</a>
+                        <a href="">Favorites</a>
+                        <a href="">Watchlist</a>
+                        <a href="">My Collections</a>
+                        <a href="">Settings</a>
+                        <h3>Stats</h3>
+                        <a href="">Rankings</a>
+                        <a href="">Activity</a>
+                    </div>
+                    <div className='links'>
+                        <h3>Resources</h3>
+                        <a href="">Help Center</a>
+                        <a href="">Platform Status</a>
+                        <a href="">Partners</a>
+                        <a href="">Gas-Free Marketplace</a>
+                        <a href="">Taxes</a>
+                        <a href="">Blog</a>
+                        <a href="">Docs</a>
+                        <a href="">Newsletter</a>
+                    </div>
+                    <div className='links'>
+                        <h3>Company</h3>
+                        <a href="">About</a>
+                        <a href="">Careers</a>
+                        <a href="">Ventures</a>
+                        <a href="">Grants</a>
+                        <h3>Policy</h3>
+                        <a href="">Privacy Policy</a>
+                        <a href="">Terms of Service</a>
+                    </div>
+                </div>
+                <div className="bottom-line">
+                    <div className="copywrite">
+                        <p>2022 - All rights Reserved © APYNIF</p>
+                    </div>
+                    <div className="socials">
+                       <a href=""><img src={instagram} alt="" /></a> 
+                       <a href=""><img src={facebook} alt="" /></a> 
+                       <a href=""><img src={Linkedin} alt="" /></a> 
+                       <a href=""><img src={Twitter} alt="" /></a> 
+                    </div>
+                </div>
+            </div>
+        </footer>
+        );
+    }
