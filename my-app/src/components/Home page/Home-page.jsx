a6b64915789c561c4e7fcea01e0fdc955a06ce6b
Roman Chaus <roman1997lviv@gmail.com>
2022-07-05 22:10:40 +0300

    rendering small cards from array added

--- my-app/src/components/Home page/Home-page.jsx
+++ my-app/src/components/Home page/Home-page.jsx
@@ -2,13 +2,44 @@ import React from "react";
 import  css from './home-page.module.css';
 import { SmallCard } from '../SmallCard/small-card';
 import layer from '../../img/Layer.png';
-import bigcard from '../../img/bigcard.png';
-import homepageimg2 from '../../img/homepageimg2.png';
-import homepageimg3 from '../../img/homepageimg3.png';
-import homepageimg4 from '../../img/homepageimg4.png';
 
 
 export const HomePage = (props) => {
+    const smallCardsArray = [
+        {
+            id: 1,
+            name: 'Cute Morning Cafe',
+            price: 40.5,
+            currency: 'SOL',
+            author: 'Billy Harrington',
+            imagePath: 'https://i.pinimg.com/originals/4b/82/19/4b821981c5804b3690d1bdc141ce9859.png'
+        },
+        {
+            id: 2,
+            name: 'Good Evening Cafe',
+            price: 50.5,
+            currency: 'SOL',
+            author: 'Roman Chaus',
+            imagePath: 'https://i.pinimg.com/originals/4b/82/19/4b821981c5804b3690d1bdc141ce9859.png'
+        },
+        {
+            id: 3,
+            name: 'Good Afternoon Cafe',
+            price: 60.5,
+            currency: 'SOL',
+            author: 'Adolf Dakh',
+            imagePath: 'https://i.pinimg.com/originals/4b/82/19/4b821981c5804b3690d1bdc141ce9859.png'
+        },
+        {
+            id: 4,
+            name: 'Cute Night Cafe',
+            price: 70.5,
+            currency: 'SOL',
+            author: 'Ben Stiller',
+            imagePath: 'https://i.pinimg.com/originals/4b/82/19/4b821981c5804b3690d1bdc141ce9859.png'
+        }
+    ];
+
     return (
         <div className={css.main}>
             <img className={css.layer} src={layer} alt="layer" />
@@ -17,34 +48,16 @@ export const HomePage = (props) => {
                     extraordinary <strong>NFTs</strong></h1>
             </div>
             <div className={css.cards}>
-            <SmallCard
-                name="Cute Morning Cafe" 
-                price="30.5" 
-                currency="SOL" 
-                author="Ben Colfex"
-                imagePath={bigcard}>
-            </SmallCard>
-           <SmallCard
-                name="Cute Morning Cafe" 
-                price="30.5" 
-                currency="SOL" 
-                author="Ben Colfex"
-                imagePath={homepageimg2}>
-            </SmallCard>
-            <SmallCard
-                name="Cute Morning Cafe" 
-                price="30.5" 
-                currency="SOL" 
-                author="Ben Colfex"
-                imagePath={homepageimg3}>
-            </SmallCard>
-            <SmallCard
-                name="Cute Morning Cafe" 
-                price="30.5" 
-                currency="SOL" 
-                author="Ben Colfex"
-                imagePath={homepageimg4}>
-            </SmallCard>
+                {smallCardsArray.map(({ id, name, price, currency, author, imagePath }) => (
+                    <SmallCard
+                        key={id}
+                        name={name}
+                        price={price}
+                        currency={currency}
+                        author={author}
+                        imagePath={imagePath}>
+                    </SmallCard>
+                ))}
             </div>
         </div>
     )
