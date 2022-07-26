ca46016b779b21404fbf7222f9607ab22e101a29
Roman Chaus <roman1997lviv@gmail.com>
2022-07-26 12:30:14 +0300

    "Header" changed to "header.jsx"

--- my-app/src/App.js
+++ my-app/src/App.js
@@ -1,5 +1,5 @@
 import "./App.css";
-import Header from "./components/Header/Header";
+import Header from "./components/Header/header.jsx";
 import { SmallCard } from "./components/SmallCard/small-card.jsx";
 import smallcard from "./img/smallcard.png";
 import { Footer } from "./components/Footer/footer.jsx";
